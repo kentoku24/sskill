7246e88105954705e98055fbaac359f0587c5d85
kentoku24 <kentoku.matsunami@gmail.com>
2018-04-20 18:30:36 +0900

    init

---
+++ sskill.bat
@@ -0,0 +1,6 @@
+taskkill /im SKYSEAHS.exe /F /T
+taskkill /im CtlCli.exe /F /T
+taskkill /im CtlCli64.exe /F /T
+taskkill /im SvcMon.exe /F /T
+taskkill /im LogChromeNativeHost.exe /F /T
+taskkill /im LogFxNativeHost.exe /F /T
